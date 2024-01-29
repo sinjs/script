151a5ebc0f3640c948600aeb656b8601bcede127
sin <50749166+sinjs@users.noreply.github.com>
2024-01-29 18:37:31 +0100

    add script

--- script.sh
+++ script.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "hello"
+CONFIG=config.json
+CREDS=credentials.json
+
+echo "Downloading configurations"
+
+curl -Lso "$CONFIG" https://raw.githubusercontent.com/sinjs/script/shitlaptop-arch/$CONFIG
+curl -Lso "$CONFIG" https://raw.githubusercontent.com/sinjs/script/shitlaptop-arch/$CREDS
+
+echo "Running archinstall"
+echo "Config: $CONFIG"
+echo "Creds: $CREDS"
+
+archinstall --config $CONFIG --creds $CREDS

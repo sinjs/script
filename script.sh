409482e2a5a8e1bbca8e5843274576269a9be2c4
sin <50749166+sinjs@users.noreply.github.com>
2024-01-29 18:40:29 +0100

    fix bug

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ CREDS=credentials.json
 echo "Downloading configurations"
 
 curl -Lo "$CONFIG" https://raw.githubusercontent.com/sinjs/script/shitlaptop-arch/$CONFIG
-curl -Lo "$CONFIG" https://raw.githubusercontent.com/sinjs/script/shitlaptop-arch/$CREDS
+curl -Lo "$CREDS" https://raw.githubusercontent.com/sinjs/script/shitlaptop-arch/$CREDS
 
 echo "Running archinstall"
 echo "Config: $CONFIG"
